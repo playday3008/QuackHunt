8cdce8418ab720c706c3ed81bd6e3cdc3f98aee4
Olala53 <aleksandra.jasnosz@gmail.com>
2024-01-19 20:21:00 +0100

    Update index.js

--- src/game/index.js
+++ src/game/index.js
@@ -1,3 +1,99 @@
+// ##################### CORE #####################
+class GameCore {
+  shots = 0;
+  hits = 0;
+  objects = {};
+
+  constructor(data) {
+      this.data = data;
+  }
+
+  tick() {
+      fill(config.dark ? 255 : 0);
+      text('Accuracy: ' + (this.shots ? Math.round(this.hits / this.shots * 100) : 100) + '%', 10, windowHeight - 10);
+
+      let shot = this.data.shot;
+      if (shot !== null) {
+          this.shots++;
+          if (assets.shots[randomInt(assets.shots.length)]) {
+              assets.shots[randomInt(assets.shots.length)].play();
+          };
+          if (shot == '000' || !(shot in this.objects)) {
+              // miss
+          } else {
+              this.objects[shot].hit = true;
+              if (assets.shots[randomInt(assets.shots.length)]) {
+                  assets.hits[randomInt(assets.hits.length)].play();
+              };
+              this.hits++;
+          }
+          this.data.shot = null;
+      }
+  }
+};
+
+// ##################### OBJECTS #####################
+class BirdsObj {
+  hit = false;
+  constructor(r, w, h, s) {
+      this.escaped = false;
+      this.w = w;
+      this.h = h;
+      this.size = w * Difficulties[config.difficulty].size;
+      this.col = '#' + Colors.codes[r];
+      this.x = random(windowWidth * (0.5 + 0.3), windowWidth * (0.5 - 0.3));
+      this.y = windowHeight / 2 + h / 2 - this.size;
+      this.speed = Math.floor(random(1, 6)) * 5 * Difficulties[config.difficulty].speed / 100;
+      this.speedm = Math.floor(6) * 5 * Difficulties[config.difficulty].speed / 100;
+
+      this.v = createVector(1, 1);
+      this.v.setHeading(random(-(0.5 + 0.1) * PI, -(0.5 - 0.1) * PI));
+      this.v.setMag(this.speed);
+
+      this.seed = random();
+      this.frame = 0;
+      this.sprites = s;
+  }
+  draw() {
+      if (frameCount % (this.speedm - this.speed) == 0) {
+          if (++this.frame > 3) this.frame = 0;
+      }
+
+      let sprite = this.sprites[this.frame].get();
+      sprite.resize(this.size, 0);
+      imageMode(CENTER);
+
+      if (this.v.x < 0) {
+          push();
+          scale(-1, 1);
+          image(sprite, -this.x, this.y);
+          pop();
+      } else {
+          image(sprite, this.x, this.y);
+      }
+
+      this.x += this.v.x;
+      this.y += this.v.y;
+
+      if (!this.escaped) {
+          if (this.y + this.size / 2 < windowHeight / 2 + this.h / 2 - this.h / 6) this.escaped = true;
+          return;
+      }
+
+      this.seed += 0.06;
+      this.v.rotate((noise(this.seed) - 0.5) * PI / 10);
+
+      if (this.x + this.size / 2 > (windowWidth + this.w) / 2 || this.x - this.size / 2 < (windowWidth - this.w) / 2) {
+          this.x -= this.v.x;
+          this.v.x = -this.v.x;
+      }
+      if (this.y + this.size / 2 > windowHeight / 2 + this.h / 2 - this.h / 6 || this.y - this.size / 2 < (windowHeight - this.h) / 2) {
+          this.y -= this.v.y;
+          this.v.y = -this.v.y;
+      }
+  }
+};
+
 // ##################### CONSTANTS #####################
 const Difficulties = [
   { speed: 20, size: 0.15 }, // CAN I PLAY, DADDY?
@@ -23,6 +119,9 @@ const Colors = {
 const BirdsVariety = ['Red', 'Yellow', 'Blue', 'Magenta'];
 
 // ##################### GLOBALS #####################
+let data = {
+  shot: null,
+};
 
 let assets = {
   shots: [],
@@ -41,6 +140,8 @@ let config = {
   ip: '192.168.1.101',
   treshold: 50,
 };
+
+let game = null;
 
 let packet = null;
 
@@ -104,6 +205,14 @@ function setup() {
           'Über': 4,
           'Mein Leben': 5,
       },
+  }).on('change',() => {
+    if (game) {
+    };
+  });
+
+  pane.addButton({
+    title: 'Start',
+  }).on('click', () => {
   });
 
   pane_player = pane.addFolder({ title: config.name, expanded: true });
@@ -113,10 +222,14 @@ function setup() {
 }
 
 function draw() {
-
+  if (game) game.tick();
 }
 
 // ##################### MISC #####################
+function randomInt(max, mmax = null) {
+  if (mmax === null) return Math.floor(Math.random() * max);
+  else return Math.random() * (mmax - max) + max;
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
